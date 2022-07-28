45d6b5e5051e981f23fb254e7376f47b201be032
S A Saharukh <sasaharukh@gmail.com>
2022-07-28 18:27:56 +0600

    enable cros

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors');
+
 
 app.use(express.json());
 
+app.use(cors({
+  origin: '*'
+}));
+
 app.post('/generate', (req, res) => {
 
+
+  console.log(req.body);
    const {string , letterToRemove} = req.body;
+
+   if(!string){
+   return res.status(400). send({error: 'Please provide a string'});
+   }
+   if(!letterToRemove){
+    return res.status(400). send({error: 'Please provide a letter to remove'});
+   }
+
 
    const result = string.replaceAll(letterToRemove,"")
   res.send({result})
